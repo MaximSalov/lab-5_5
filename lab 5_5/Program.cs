5a4602dc8050dcc754d6f59da4312f3fb3d91c02
ПОльзователь <@USER436809363>
2021-10-27 15:55:55 +0300

    Добавьте файлы проекта.

---
+++ lab 5_5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_5_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число n");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[,] t = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if ((i + j) % 2 == 0)
+                        t[i, j] = 1;
+                    Console.Write("{0,4}", t[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
+       
+
+        
